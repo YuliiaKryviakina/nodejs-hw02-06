f30c808e235afda8bb058997cbe669448eee72ac
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-09-08 00:00:37 +0300

    Revert "get funct"
    This reverts commit adca3a70da835558584c365d7863ecf90f6ffe62.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,29 +1,25 @@
-const express = require("express");
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
-const { listContacts, getContactById } = require("../../models/contacts");
+router.get('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.get("/", async (_, res) => {
-  const contacts = await listContacts();
-  res.status(200).json(contacts);
-});
+router.get('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.get("/:contactId", async (req, res, next) => {
-  const contact = await getContactById(req.params.contactId);
-  res.status(200).json(contact);
-});
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.put('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
-
-module.exports = router;
+module.exports = router
