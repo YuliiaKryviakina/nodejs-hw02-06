38ca3d053a15a1b691da69a9429124b1692a1074
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-09-07 22:31:28 +0300

    rotes refact

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,78 +1,24 @@
 const express = require("express");
+const controllers = require("../../controllers/contactsControllers");
 
 const router = express.Router();
 
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../../models/contacts");
+// const {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// } = require("../../models/contacts");
 
-router.get("/", async (_, res) => {
-  const contacts = await listContacts();
-  res.status(200).json(contacts);
-});
+router.get("/", controllers.listContacts);
 
-router.get("/:contactId", async (req, res, next) => {
-  const contact = await getContactById(req.params.contactId);
+router.get("/:contactId", controllers.getContactById);
 
-  if (!contact) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
-  res.status(200).json(contact);
-});
+router.post("/", controllers.addContact);
 
-router.post("/", async (req, res, next) => {
-  const contact = await addContact(req.body);
+router.delete("/:contactId", controllers.removeContact);
 
-  // if (!newContact) {
-  //   res.status(400).json({ message: "missing required name field" });
-  //   return;
-  // }
-  if (typeof contact === "string") {
-    const errorMessage = `missing required ${contact} field`;
-    // console.log(contact);
-    res.status(400).json({ message: errorMessage });
-    return;
-  }
-  res.status(201).json(contact);
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  const contact = await removeContact(req.params.contactId);
-  if (!contact) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
-  res.status(200).json({ message: "contact deleted" });
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  const contact = await updateContact(req.params.contactId, req.body);
-
-  if (Object.keys(req.body).length === 0) {
-    res.status(400).json({ message: "missing fields" });
-    return;
-  }
-  console.log(`contact: ${contact}`);
-
-  if (contact === null) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
-
-  // if (contact === undefined) {
-  //   res.status(400).json({ message: "missing fields" });
-  if (typeof contact === "string") {
-    const errorMessage = `missing required ${contact} field`;
-    res.status(400).json({ message: errorMessage });
-    return;
-  }
-
-  res.status(200).json(contact);
-});
+router.put("/:contactId", controllers.updateContact);
 
 module.exports = router;
