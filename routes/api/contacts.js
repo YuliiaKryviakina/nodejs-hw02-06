57a21cf8ac07cf12973878b8668b17e3866441fb
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-09-07 22:48:48 +0300

    validation refact

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const controllers = require("../../controllers/contactsControllers");
 
 const router = express.Router();
+const validateBody = require("../../middleware/validateBody");
 
 // const {
 //   listContacts,
@@ -10,15 +11,20 @@ const router = express.Router();
 //   addContact,
 //   updateContact,
 // } = require("../../models/contacts");
+const schemas = require("../../schemas/contacts");
 
 router.get("/", controllers.listContacts);
 
 router.get("/:contactId", controllers.getContactById);
 
-router.post("/", controllers.addContact);
+router.post("/", validateBody(schemas.addSchema), controllers.addContact);
 
 router.delete("/:contactId", controllers.removeContact);
 
-router.put("/:contactId", controllers.updateContact);
+router.put(
+  "/:contactId",
+  validateBody(schemas.addSchema),
+  controllers.updateContact
+);
 
 module.exports = router;
