e17ef46f8206a951f00ea449e4e62e6e407e3e4e
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-09-08 00:00:33 +0300

    Revert "add validate"
    This reverts commit aaa0f8caa9369b27c76a1ad6bb01a50a28c4c56f.

--- models/contacts.js
+++ models/contacts.js
@@ -1,8 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
 const contactsPath = path.join(__dirname, "contacts.json");
-const { nanoid } = require("nanoid");
-const dataValidator = require("../helpers/dataValidator");
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath, "utf-8");
@@ -19,54 +17,11 @@ const getContactById = async (contactId) => {
   return contactById[0];
 };
 
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const restList = await contacts.filter((contact) => contact.id !== contactId);
+const removeContact = async (contactId) => {};
 
-  const index = contacts.findIndex((element) => element.id === contactId);
-  if (index === -1) {
-    return null;
-  }
+const addContact = async (body) => {};
 
-  const stringifiedData = [JSON.stringify(restList, null, 2)];
-  fs.writeFile(contactsPath, stringifiedData);
-  return restList;
-};
-
-const addContact = async (body) => {
-  const { name, email, phone } = body;
-  const validatedData = dataValidator({ name, email, phone });
-  if (validatedData.error) {
-    console.log(validatedData.error);
-    return null;
-  }
-
-  const contacts = await listContacts();
-  const newContact = { id: nanoid(), ...{ name, email, phone } };
-  contacts.push(newContact);
-
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
-};
-
-const updateContact = async (contactId, body) => {
-  const contacts = await listContacts();
-
-  const { name, email, phone } = body;
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  const validatedData = dataValidator({ name, email, phone });
-
-  if (validatedData.error) {
-    return undefined;
-  }
-
-  contacts[index] = { id: contactId, ...body };
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return contacts[index];
-};
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,13 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../../models/contacts");
+const { listContacts, getContactById } = require("../../models/contacts");
 
 router.get("/", async (_, res) => {
   const contacts = await listContacts();
@@ -17,47 +11,19 @@ router.get("/", async (_, res) => {
 
 router.get("/:contactId", async (req, res, next) => {
   const contact = await getContactById(req.params.contactId);
-
-  if (!contact) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
   res.status(200).json(contact);
 });
 
 router.post("/", async (req, res, next) => {
-  const newContact = await addContact(req.body);
-
-  if (!newContact) {
-    res.status(400).json({ message: "missing required name field" });
-    return;
-  }
-  res.status(201).json(newContact);
+  res.json({ message: "template message" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  const contact = await removeContact(req.params.contactId);
-  if (!contact) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
-  res.status(200).json({ message: "contact deleted" });
+  res.json({ message: "template message" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  const contact = await updateContact(req.params.contactId, req.body);
-
-  if (contact === null) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
-
-  if (contact === undefined) {
-    res.status(400).json({ message: "missing fields" });
-    return;
-  }
-
-  res.status(200).json(contact);
+  res.json({ message: "template message" });
 });
 
 module.exports = router;
