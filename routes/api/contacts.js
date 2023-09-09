a66865ca1d8f1011879385721105d58fe774c7d5
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-09-09 04:06:44 +0300

    after check

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,24 +9,29 @@ const {
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  getAll(req, res, next);
-});
+// router.get("/", (req, res, next) => {
+//   getAll(req, res, next);
+// });
+router.get("/", getAll);
 
-router.get("/:contactId", (req, res, next) => {
-  getById(req, res, next);
-});
+// router.get("/:contactId", (req, res, next) => {
+//   getById(req, res, next);
+// });
+router.get("/:contactId", getById);
 
-router.post("/", (req, res, next) => {
-  create(req, res, next);
-});
+// router.post("/", (req, res, next) => {
+//   create(req, res, next);
+// });
+router.post("/", create);
 
-router.put("/:contactId", (req, res, next) => {
-  update(req, res, next);
-});
+// router.put("/:contactId", (req, res, next) => {
+//   update(req, res, next);
+// });
+router.put("/:contactId", update);
 
-router.delete("/:contactId", (req, res, next) => {
-  deleteById(req, res, next);
-});
+// router.delete("/:contactId", (req, res, next) => {
+//   deleteById(req, res, next);
+// });
+router.delete("/:contactId", deleteById);
 
 module.exports = router;
--- utils/validation/contactsValidation.js
+++ utils/validation/contactsValidation.js
@@ -1,25 +1,44 @@
 const Joi = require("joi");
 
-const namePattern = /^[\p{Script=Latin}\p{Script=Cyrillic}\s]*$/u;
-const phonePattern = /^[0-9 ()+-]+$/;
+// const namePattern = /^[\p{Script=Latin}\p{Script=Cyrillic}\s]*$/u;
+// const phonePattern = /^[0-9 ()+-]+$/;
 
+// const schemaCreateContact = Joi.object({
+//   name: Joi.string().pattern(namePattern).min(3).max(30).required(),
+//   email: Joi.string()
+//     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+//     .required(),
+//   phone: Joi.string().pattern(phonePattern).min(7).max(18).required(),
+// }).options({ abortEarly: false });
 const schemaCreateContact = Joi.object({
-  name: Joi.string().pattern(namePattern).min(3).max(30).required(),
+  name: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required name field" }),
+
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-  phone: Joi.string().pattern(phonePattern).min(7).max(18).required(),
-}).options({ abortEarly: false });
+    .required()
+    .messages({ "any.required": "missing required email field" }),
+  phone: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required phone field" }),
+});
+
+// const schemaUpdateContact = Joi.object({
+//   name: Joi.string().pattern(namePattern).min(3).max(30).optional(),
+//   email: Joi.string()
+//     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+//     .required(),
+//   phone: Joi.string().pattern(phonePattern).min(7).max(18).optional(),
+// })
+//   .or("name", "email", "phone")
+//   .options({ abortEarly: false });
 
 const schemaUpdateContact = Joi.object({
-  name: Joi.string().pattern(namePattern).min(3).max(30).optional(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-  phone: Joi.string().pattern(phonePattern).min(7).max(18).optional(),
-})
-  .or("name", "email", "phone")
-  .options({ abortEarly: false });
+  name: Joi.string().required().messages({ "any.required": "missing fields" }),
+
+  email: Joi.string().required().messages({ "any.required": "missing fields" }),
+  phone: Joi.string().required().messages({ "any.required": "missing fields" }),
+});
 
 module.exports = {
   schemaCreateContact,
